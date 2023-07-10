346d0f6dde98890f3ac2d9efcd51fdcb2d14c9b4
unknown <mariamatef226@gmail.com>
2023-07-10 22:24:07 +0300

    added a new project

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ window.onload = function () {
   // When adding new project, introduce it in each of the following 3 structures:
 
   // structure 1
-  const projects = ["House Wallet", "Sure Wheels", "BookArt Shop", "Pour Elle", "Ducktionary", "Tic-Tac-Toe", "What's Your Age"];
+  const projects = ["House Wallet", "Sure Wheels", "Binge Watcher Test", "BookArt Shop", "Pour Elle", "Ducktionary", "Tic-Tac-Toe", "What's Your Age"];
 
   // structure 2
   const projectSource = Array();
@@ -50,6 +50,8 @@ window.onload = function () {
     "https://www.youtube.com/embed/D3pLPoz0UhA?autoplay=1&mute=1&controls=0&loop=1";
   projectSource["BookArt Shop"] =
     "https://mariamatef226.github.io/bookartalex/";
+  projectSource["Binge Watcher Test"] =
+    "https://mariamatef226.github.io/binge-watcher-test/";
   projectSource["Pour Elle"] = "https://mariamatef226.github.io/pourelle/";
   projectSource["Ducktionary"] =
     "https://www.youtube.com/embed/mOdyblwMmNg?autoplay=1&mute=1&controls=0&loop=1";
@@ -67,7 +69,7 @@ window.onload = function () {
   4.Allowing user to modify their budget details, such as initial budget and consumption alert values.<br>\
   5.Storing the net budget of the previous months, allowing users to track and adjust their spending habits.<br>\
   The application UI is built on desktop-first design basis. It's responsive on all devices.<br>\
-  <a class='text-decoration-none text-info' href='http://house-wallet.epizy.com/'>Pay a visit to the Website? Click here!</a>";
+  <a class='text-decoration-none text-info' target='_blank' href='http://house-wallet.epizy.com/'>Pay a visit to the Website? Click here!</a>";
 
   aboutProject["Sure Wheels"] =
     "Sure Wheels is a dynamic website for a virtual car rental business, where a customer\
@@ -76,23 +78,28 @@ window.onload = function () {
    The database is implemented using RDBMS - MySQL and the backend is implemented using PHP. The frontend was done\
     using HTML, CSS, Bootstrap4/5 and JavaScript. It was a team project for uni. I was responsible for the backend portion,\
      in addition to applying responstivity to the website.<br>\
-     <a class='text-decoration-none text-info'href='wheels.000webhostapp.com/customer_module/home.php'>Pay a visit to the Website? Click here!</a>\
+     <a class='text-decoration-none text-info target='_blank' 'href='wheels.000webhostapp.com/customer_module/home.php'>Pay a visit to the Website? Click here!</a>\
      <br> <i class='text-warning' style='font-size:1rem;'>PS: use VPN, since 000webhost isn't working on all ISPs</i>";
-     
+  
+     aboutProject["Binge Watcher Test"] = "A simple quiz app using React.js. This solo project is the final project for Bob Ziroll's 'Learn React' course on Scrimba.\
+     <a class='text-decoration-none link-info' target='_blank' href='https://mariamatef226.github.io/binge-watcher-test/'>Pay a visit to the Website? Click here!</a>";
+
 
   aboutProject["BookArt Shop"] =
     "BookArt Shop is a static business portfolio for my small business, BookArt Shop. The website includes information\
    about the owner and the business itself, in addition to displaying the products sold. Stickers & Other Products pages are still under\
     construction. The wesbite is responsive on all devices. It was implemented using native tools (HTML, CSS, JavaScript & Bootstrap 5).\
      The website follows Bondi design. It's an individual personal project.<br>\
-     <a class='text-decoration-none text-info' href='https://mariamatef226.github.io/bookartalex/'>Pay a visit to the Website? Click here!</a>";
+     <a class='text-decoration-none text-info' target='_blank' href='https://mariamatef226.github.io/bookartalex/'>Pay a visit to the Website? Click here!</a>";
 
   aboutProject["Pour Elle"] =
     "Pour Elle is a static website for women care that uses Bondi design scheme. The website is still under construction.\
    It was implemented using HTML, CSS, Bootstrap5 & JavaScript. It was a final project for an online frontend course and it was an individual project.\
   Responsivness is supported on all devices.<br>\
-  <a class='text-decoration-none link-info' href='https://mariamatef226.github.io/pourelle/'>Pay a visit to the Website? Click here!</a>";
+  <a class='text-decoration-none link-info' target='_blank' href='https://mariamatef226.github.io/pourelle/'>Pay a visit to the Website? Click here!</a>";
 
+  
+  
   aboutProject["Ducktionary"] =
     "A Python desktop application of an offline english dictionary. Useful in case of lack of internet connection for people who are studying\
    English or reading a book & have met a word they're unfamiliar with and would like to know its meaning (defintion). The user would enter the meant word, and the\
@@ -102,12 +109,12 @@ window.onload = function () {
   aboutProject["Tic-Tac-Toe"] = 
   "A simple Tic-Tac-Toe web game, where player plays against the app. Choices by the app are randomly developed, i'ts not AI based. It was a part of a JavaScript course.\
   The game is responsive and playable on all screens.<br>\
-  <a class='text-decoration-none text-info'href='https://mariamatef226.github.io/tic-tac-toe/game.html'>Pay a visit to the Website? Click here!</a>";
+  <a class='text-decoration-none text-info' target='_blank' href='https://mariamatef226.github.io/tic-tac-toe/game.html'>Pay a visit to the Website? Click here!</a>";
 
   aboutProject["What's Your Age"] ="An online accurate age calculator, to which you enter your detailed date of birth (including year, month, day, hour, minute\
   and second if you wish). Afterwards, it'll display to you a very accurate age of yours (also in year, month, day, hour, minute and second).\
   The calculator works and responsive on all devices. Interactivity is done using JavaScript. <br>\
-  <a class='text-decoration-none text-info'href='https://mariamatef226.github.io/what-is-your-age/index.html'>Pay a visit to the Website? Click here!</a>";
+  <a class='text-decoration-none text-info' target='_blank' href='https://mariamatef226.github.io/what-is-your-age/index.html'>Pay a visit to the Website? Click here!</a>";
 
   
 
