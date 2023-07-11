381b464f270725f2201adf57c01d344a96236c62
unknown <mariamatef226@gmail.com>
2023-07-11 03:26:59 +0300

    Fixed JavaScript Bugs + moved data to module + refactored code

---
+++ js/projects.js
@@ -0,0 +1,83 @@
+export default [
+  {
+    name: "House Wallet",
+    source:
+      "https://www.youtube.com/embed/oWncRAk1Ga0?autoplay=1&mute=1&controls=0&loop=1",
+    about:
+      "A web-based application that helps users to manage their household budgets effectively where it provides users with the ability to track their expenses, set budgets, and monitor their spending habits. It includes the following features:<br>\
+    1.Database for storing multiple users data where a user can login to his account from any device, and more than one user can track their budgets using the same device.<br>\
+    2.Storing purchases for users who can view and filter them in a list format.<br>\
+    3.Providing users with a summary of their total remaining, consumed, and original budget.<br>\
+    4.Allowing user to modify their budget details, such as initial budget and consumption alert values.<br>\
+    5.Storing the net budget of the previous months, allowing users to track and adjust their spending habits.<br>\
+    The application UI is built on desktop-first design basis. It's responsive on all devices.<br>\
+    <a class='text-decoration-none text-info' target='_blank' href='http://house-wallet.epizy.com/'>Pay a visit to the Website? Click here!</a>",
+  },
+  {
+    name: "Sure Wheels",
+    source:
+      "https://www.youtube.com/embed/D3pLPoz0UhA?autoplay=1&mute=1&controls=0&loop=1",
+    about:
+      "Sure Wheels is a dynamic website for a virtual car rental business, where a customer\
+    can view and rent cars. Also, they can place a review upon their rental experiences.\
+     <br> The website includes an admin module for the owners to monitor reservations, revenue and other business data.\
+    The database is implemented using RDBMS - MySQL and the backend is implemented using PHP. The frontend was done\
+     using HTML, CSS, Bootstrap4/5 and JavaScript. It was a team project for uni. I was responsible for the backend portion,\
+      in addition to applying responstivity to the website.<br>\
+      <a class='text-decoration-none text-info target='_blank' 'href='wheels.000webhostapp.com/customer_module/home.php'>Pay a visit to the Website? Click here!</a>\
+      <br> <i class='text-warning' style='font-size:1rem;'>PS: use VPN, since 000webhost isn't working on all ISPs</i>",
+  },
+  {
+    name: "Binge Watcher Test",
+    source: "https://mariamatef226.github.io/binge-watcher-test/",
+    about:
+      "A simple quiz app using React.js. This solo project is the final project for Bob Ziroll's 'Learn React' course on Scrimba.\
+    <a class='text-decoration-none link-info' target='_blank' href='https://mariamatef226.github.io/binge-watcher-test/'>Pay a visit to the Website? Click here!</a>",
+  },
+  {
+    name: "BookArt Shop",
+    source: "https://mariamatef226.github.io/bookartalex/",
+    about:
+      "BookArt Shop is a static business portfolio for my small business, BookArt Shop. The website includes information\
+    about the owner and the business itself, in addition to displaying the products sold. Stickers & Other Products pages are still under\
+     construction. The wesbite is responsive on all devices. It was implemented using native tools (HTML, CSS, JavaScript & Bootstrap 5).\
+      The website follows Bondi design. It's an individual personal project.<br>\
+      <a class='text-decoration-none text-info' target='_blank' href='https://mariamatef226.github.io/bookartalex/'>Pay a visit to the Website? Click here!</a>",
+  },
+  {
+    name: "Pour Elle",
+    source: "https://mariamatef226.github.io/pourelle/",
+    about:
+      "Pour Elle is a static website for women care that uses Bondi design scheme. The website is still under construction.\
+    It was implemented using HTML, CSS, Bootstrap5 & JavaScript. It was a final project for an online frontend course and it was an individual project.\
+   Responsivness is supported on all devices.<br>\
+   <a class='text-decoration-none link-info' target='_blank' href='https://mariamatef226.github.io/pourelle/'>Pay a visit to the Website? Click here!</a>",
+  },
+  {
+    name: "Ducktionary",
+    source:
+      "https://www.youtube.com/embed/mOdyblwMmNg?autoplay=1&mute=1&controls=0&loop=1",
+    about:
+      "A Python desktop application of an offline english dictionary. Useful in case of lack of internet connection for people who are studying\
+    English or reading a book & have met a word they're unfamiliar with and would like to know its meaning (defintion). The user would enter the meant word, and the\
+     application would display its english definition.<br> The application is implemented using Python 3. The algorithm is based on Red-black trees\
+     data structure, which supports good response time. Finally, the source of the dictionary is Merriam-Webster's dictionary.",
+  },
+  {
+    name: "Tic-Tac-Toe",
+    source: "https://mariamatef226.github.io/tic-tac-toe/game.html",
+    about:
+      "A simple Tic-Tac-Toe web game, where player plays against the app. Choices by the app are randomly developed, i'ts not AI based. It was a part of a JavaScript course.\
+    The game is responsive and playable on all screens.<br>\
+    <a class='text-decoration-none text-info' target='_blank' href='https://mariamatef226.github.io/tic-tac-toe/game.html'>Pay a visit to the Website? Click here!</a>",
+  },
+  {
+    name: "What's Your Age",
+    source: "https://mariamatef226.github.io/what-is-your-age/",
+    about:
+      "An online accurate age calculator, to which you enter your detailed date of birth (including year, month, day, hour, minute\
+        and second if you wish). Afterwards, it'll display to you a very accurate age of yours (also in year, month, day, hour, minute and second).\
+        The calculator works and responsive on all devices. Interactivity is done using JavaScript. <br>\
+        <a class='text-decoration-none text-info' target='_blank' href='https://mariamatef226.github.io/what-is-your-age/index.html'>Pay a visit to the Website? Click here!</a>",
+  },
+];
--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+import projects from "./projects.js";
+
 window.onscroll = function () {
   var name = document.querySelector(".brand-name");
   // Get the landing div element
@@ -20,282 +22,7 @@ function checkvp(bounding) {
   }
 }
 
-window.onload = function () {
-  //Typewriter effect in landing
-
-  str1 = "Hello World!";
-  str2 = "I'm Mariam Atef.";
-  str3 = "A web developer based in Egypt.";
-  landingline1 = document.querySelector(".l1");
-  landingline2 = document.querySelector(".l2");
-  landingline3 = document.querySelector(".l3");
-
-  flag = 1;
-  ptr = 1;
-
-  myInterval = setInterval(typewriter, 80);
-
-  // -- Loading Projects in Project Section --
-
-  // When adding new project, introduce it in each of the following 3 structures:
-
-  // structure 1
-  const projects = ["House Wallet", "Sure Wheels", "Binge Watcher Test", "BookArt Shop", "Pour Elle", "Ducktionary", "Tic-Tac-Toe", "What's Your Age"];
-
-  // structure 2
-  const projectSource = Array();
-  projectSource["House Wallet"] =
-  "https://www.youtube.com/embed/oWncRAk1Ga0?autoplay=1&mute=1&controls=0&loop=1";
-  projectSource["Sure Wheels"] =
-    "https://www.youtube.com/embed/D3pLPoz0UhA?autoplay=1&mute=1&controls=0&loop=1";
-  projectSource["BookArt Shop"] =
-    "https://mariamatef226.github.io/bookartalex/";
-  projectSource["Binge Watcher Test"] =
-    "https://mariamatef226.github.io/binge-watcher-test/";
-  projectSource["Pour Elle"] = "https://mariamatef226.github.io/pourelle/";
-  projectSource["Ducktionary"] =
-    "https://www.youtube.com/embed/mOdyblwMmNg?autoplay=1&mute=1&controls=0&loop=1";
-  projectSource["Tic-Tac-Toe"] =
-    "https://mariamatef226.github.io/tic-tac-toe/game.html";
-  projectSource["What's Your Age"] = "https://mariamatef226.github.io/what-is-your-age/";  
-
-  // structure 3
-  const aboutProject = Array();
-  aboutProject["House Wallet"] =
-  "A web-based application that helps users to manage their household budgets effectively where it provides users with the ability to track their expenses, set budgets, and monitor their spending habits. It includes the following features:<br>\
-  1.Database for storing multiple users data where a user can login to his account from any device, and more than one user can track their budgets using the same device.<br>\
-  2.Storing purchases for users who can view and filter them in a list format.<br>\
-  3.Providing users with a summary of their total remaining, consumed, and original budget.<br>\
-  4.Allowing user to modify their budget details, such as initial budget and consumption alert values.<br>\
-  5.Storing the net budget of the previous months, allowing users to track and adjust their spending habits.<br>\
-  The application UI is built on desktop-first design basis. It's responsive on all devices.<br>\
-  <a class='text-decoration-none text-info' target='_blank' href='http://house-wallet.epizy.com/'>Pay a visit to the Website? Click here!</a>";
-
-  aboutProject["Sure Wheels"] =
-    "Sure Wheels is a dynamic website for a virtual car rental business, where a customer\
-   can view and rent cars. Also, they can place a review upon their rental experiences.\
-    <br> The website includes an admin module for the owners to monitor reservations, revenue and other business data.\
-   The database is implemented using RDBMS - MySQL and the backend is implemented using PHP. The frontend was done\
-    using HTML, CSS, Bootstrap4/5 and JavaScript. It was a team project for uni. I was responsible for the backend portion,\
-     in addition to applying responstivity to the website.<br>\
-     <a class='text-decoration-none text-info target='_blank' 'href='wheels.000webhostapp.com/customer_module/home.php'>Pay a visit to the Website? Click here!</a>\
-     <br> <i class='text-warning' style='font-size:1rem;'>PS: use VPN, since 000webhost isn't working on all ISPs</i>";
-  
-     aboutProject["Binge Watcher Test"] = "A simple quiz app using React.js. This solo project is the final project for Bob Ziroll's 'Learn React' course on Scrimba.\
-     <a class='text-decoration-none link-info' target='_blank' href='https://mariamatef226.github.io/binge-watcher-test/'>Pay a visit to the Website? Click here!</a>";
-
-
-  aboutProject["BookArt Shop"] =
-    "BookArt Shop is a static business portfolio for my small business, BookArt Shop. The website includes information\
-   about the owner and the business itself, in addition to displaying the products sold. Stickers & Other Products pages are still under\
-    construction. The wesbite is responsive on all devices. It was implemented using native tools (HTML, CSS, JavaScript & Bootstrap 5).\
-     The website follows Bondi design. It's an individual personal project.<br>\
-     <a class='text-decoration-none text-info' target='_blank' href='https://mariamatef226.github.io/bookartalex/'>Pay a visit to the Website? Click here!</a>";
-
-  aboutProject["Pour Elle"] =
-    "Pour Elle is a static website for women care that uses Bondi design scheme. The website is still under construction.\
-   It was implemented using HTML, CSS, Bootstrap5 & JavaScript. It was a final project for an online frontend course and it was an individual project.\
-  Responsivness is supported on all devices.<br>\
-  <a class='text-decoration-none link-info' target='_blank' href='https://mariamatef226.github.io/pourelle/'>Pay a visit to the Website? Click here!</a>";
-
-  
-  
-  aboutProject["Ducktionary"] =
-    "A Python desktop application of an offline english dictionary. Useful in case of lack of internet connection for people who are studying\
-   English or reading a book & have met a word they're unfamiliar with and would like to know its meaning (defintion). The user would enter the meant word, and the\
-    application would display its english definition.<br> The application is implemented using Python 3. The algorithm is based on Red-black trees\
-    data structure, which supports good response time. Finally, the source of the dictionary is Merriam-Webster's dictionary.";
-
-  aboutProject["Tic-Tac-Toe"] = 
-  "A simple Tic-Tac-Toe web game, where player plays against the app. Choices by the app are randomly developed, i'ts not AI based. It was a part of a JavaScript course.\
-  The game is responsive and playable on all screens.<br>\
-  <a class='text-decoration-none text-info' target='_blank' href='https://mariamatef226.github.io/tic-tac-toe/game.html'>Pay a visit to the Website? Click here!</a>";
-
-  aboutProject["What's Your Age"] ="An online accurate age calculator, to which you enter your detailed date of birth (including year, month, day, hour, minute\
-  and second if you wish). Afterwards, it'll display to you a very accurate age of yours (also in year, month, day, hour, minute and second).\
-  The calculator works and responsive on all devices. Interactivity is done using JavaScript. <br>\
-  <a class='text-decoration-none text-info' target='_blank' href='https://mariamatef226.github.io/what-is-your-age/index.html'>Pay a visit to the Website? Click here!</a>";
-
-  
-
-  firstProj = projects[0]; // First project reference
-
-  // ------------------------------------
-  // 1st: Small Screens:
-  // -------------------------------------
-
-  // Building NavBar:
-
-  let carouselMobile = document.querySelector(
-    "#mobileNavProjects .carousel-inner"
-  );
-  let projDisplay = document.querySelector(".disp-proj");
-  let aboutProj = document.querySelector(".about-proj");
-
-  for (let name of projects) {
-    // Create Element
-
-    var element = document.createElement("div");
-    if (name == firstProj) {
-      element.classList.add("carousel-item", "active", "text-center");
-    } else {
-      element.classList.add("carousel-item", "text-center");
-    }
-    element.innerText = name;
-
-    // Append to Container
-
-    carouselMobile.append(element);
-  }
-
-  // Displaying Currently Active Project
-
-  // A) default on load --> 1st Project
-  // get first project's attributes
-
-  // let type = projects.get(firstProj);
-  let src = projectSource[firstProj];
-
-  // load the display -- """" the else clause to be modified """"
-
-  var frame = document.createElement("iframe");
-  frame.setAttribute("src", src);
-  frame.setAttribute("width", "100%");
-  frame.setAttribute("height", "100%");
-  frame.style.margin = "0 auto";
-  frame.style.border = "solid #212529 5px";
-  projDisplay.append(frame);
-
-  // Displaying its about section
-  aboutProj.innerHTML = aboutProject[firstProj];
-
-  // B) On Changing the Slide
-
-  document
-    .querySelector("#mobileNavProjects")
-    .addEventListener("slide.bs.carousel", function (e) {
-      // fade out effect
-      projDisplay.style.animation = "fade-out 0.8s linear forwards";
-      projDisplay.style.webkitAnimation = "fade-out 0.8s linear forwards";
-
-      // delay to catch the new active slide
-
-      setTimeout(() => {
-        projDisplay.classList.add("play-fade-out");
-
-        projDisplay.innerHTML = ""; // clear container
-
-        let children = Array.from(carouselMobile.children);
-        active = children.filter((element) =>
-          element.classList.contains("active")
-        )[0].innerText;
-
-        let src = projectSource[active];
-
-        var frame = document.createElement("iframe");
-        frame.setAttribute("src", src);
-        frame.setAttribute("width", "100%");
-        frame.setAttribute("height", "100%");
-        frame.style.margin = "0 auto";
-        frame.style.border = "solid #212529 5px";
-        projDisplay.append(frame);
-
-        // fade in effect
-
-        projDisplay.style.animation = "fade-in 0.8s linear forwards";
-        projDisplay.style.webkitAnimation = "fade-in 0.8s linear forwards";
-
-        // Displaying its about section
-        aboutProj.innerHTML = aboutProject[active];
-      }, 800);
-
-
-    });
-
-  // ------------------------------------
-  // 2nd: Large Screens:
-  // -------------------------------------
-
-  // Building NavBar:
-
-  let projSideBar = document.querySelector(".side-nav-proj");
-
-  for (let name of projects) {
-    // Create Element
-
-    var element = document.createElement("button");
-    if (name == firstProj) {
-      element.classList.add("btn", "text-md-left", "active-project");
-    } else {
-      element.classList.add("btn", "text-md-left");
-    }
-    element.innerText = name;
-
-    // adding activation on click event
-    element.addEventListener("click", function () {
-      this.classList.add("active-project");
-      let innertextis = this.innerHTML;
-      let siblings = Array.from(projSideBar.children);
-      siblings.forEach((element) => {
-        if (element.innerHTML !== innertextis) {
-          element.classList.remove("active-project");
-        }
-      });
-    });
-
-    // Append to Container
-    projSideBar.append(element);
-  }
-
-  // Displaying Currently Active Project
-
-  // On Changing the tab
-
-  document
-    .querySelector(".side-nav-proj")
-    .addEventListener("click", function (e) {
-      // fade out effect
-      projDisplay.style.animation = "fade-out 0.8s linear forwards";
-      projDisplay.style.webkitAnimation = "fade-out 0.8s linear forwards";
-
-      // delay to allow animation
-
-      setTimeout(() => {
-        projDisplay.innerHTML = ""; // clear container
-
-        let children = Array.from(projSideBar.children);
-        active = children.filter((element) =>
-          element.classList.contains("active-project")
-        )[0].innerText;
-
-        let src = projectSource[active];
-
-        var frame = document.createElement("iframe");
-        frame.setAttribute("src", src);
-        frame.setAttribute("width", "100%");
-        frame.setAttribute("height", "100%");
-        frame.style.margin = "0 auto";
-        frame.style.border = "solid #212529 5px";
-        projDisplay.append(frame);
-
-        // fade in effect
-
-        projDisplay.style.animation = "fade-in 0.8s linear forwards";
-        projDisplay.style.webkitAnimation = "fade-in 0.8s linear forwards";
-
-        // Displaying its about section
-        aboutProj.innerHTML = aboutProject[active];
-
-        // adjust size of sidenav
-        //var aboutsecheight = document.querySelector(".about-proj").offsetHeight;
-        //let h = aboutsecheight + 702;
-
-      }, 800);
-
-    });
-};
-
-function typewriter() {
+function typewriter(landingline1, landingline2, landingline3) {
   switch (flag) {
     case 1:
       landingline1.innerText = str1.substring(0, ptr + 1) + "|";
@@ -328,7 +55,161 @@ function typewriter() {
   }
 }
 
-// To do:
+function buildIframe(projDisplay, src) {
+  var frame = document.createElement("iframe");
+  frame.setAttribute("src", src);
+  frame.setAttribute("width", "100%");
+  frame.setAttribute("height", "100%");
+  frame.style.margin = "0 auto";
+  frame.style.border = "solid #212529 5px";
+  projDisplay.append(frame);
+}
 
-// write content for project
-// dynamic height for project
+// variables used for typwwriter animation
+let str1 = "Hello World!";
+let str2 = "I'm Mariam Atef.";
+let str3 = "A web developer based in Egypt.";
+let flag = 1;
+let ptr = 1;
+var myInterval;
+
+window.onload = function () {
+  //Typewriter effect in landing
+  let landingline1 = document.querySelector(".l1");
+  let landingline2 = document.querySelector(".l2");
+  let landingline3 = document.querySelector(".l3");
+
+  myInterval = setInterval(
+    typewriter,
+    80,
+    landingline1,
+    landingline2,
+    landingline3
+  );
+
+  // displaying projects
+
+  var firstProj = projects[0]; // First project reference
+
+  // ------------------------------------
+  // 1st: Small Screens:
+  // -------------------------------------
+
+  // Building NavBar:
+
+  let carouselMobile = document.querySelector(
+    "#mobileNavProjects .carousel-inner"
+  );
+  let projDisplay = document.querySelector(".disp-proj");
+  let aboutProj = document.querySelector(".about-proj");
+
+  for (let project of projects) {
+    // Create Element
+    var element = document.createElement("div");
+    if (project == firstProj) {
+      element.classList.add("carousel-item", "active", "text-center");
+    } else {
+      element.classList.add("carousel-item", "text-center");
+    }
+    element.innerText = project.name;
+
+    // Append to Container
+    carouselMobile.append(element);
+  }
+
+  // Displaying Currently Active Project
+
+  // A) default on load --> 1st Project
+
+  // get first project's attributes
+  let src = firstProj.source;
+
+  // load the display
+  buildIframe(projDisplay, src);
+
+  // Displaying its about section
+  aboutProj.innerHTML = firstProj.about;
+
+  // B) On Changing the Slide
+
+  document
+    .querySelector("#mobileNavProjects")
+    .addEventListener("slide.bs.carousel", () => {
+      switchProject("mobile");
+    });
+
+  // ------------------------------------
+  // 2nd: Large Screens:
+  // -------------------------------------
+
+  // Building NavBar:
+
+  let projSideBar = document.querySelector(".side-nav-proj");
+
+  for (let project of projects) {
+    // Create Element
+
+    var element = document.createElement("button");
+    if (project == firstProj) {
+      element.classList.add("btn", "text-md-left", "active-project");
+    } else {
+      element.classList.add("btn", "text-md-left");
+    }
+    element.innerText = project.name;
+
+    // adding activation on click event
+    element.addEventListener("click", function () {
+      this.classList.add("active-project");
+      let innertextis = this.innerHTML;
+      let siblings = Array.from(projSideBar.children);
+      siblings.forEach((element) => {
+        if (element.innerHTML !== innertextis) {
+          element.classList.remove("active-project");
+        }
+      });
+    });
+
+    // Append to Container
+    projSideBar.append(element);
+  }
+
+  // Displaying Currently Active Project On Changing the tab
+
+  document.querySelector(".side-nav-proj").addEventListener("click", () => {
+    switchProject("desktop");
+  });
+
+  // handling switching between projects
+  function switchProject(parent) {
+    // fade out effect
+    projDisplay.style.animation = "fade-out 0.8s linear forwards";
+    projDisplay.style.webkitAnimation = "fade-out 0.8s linear forwards";
+
+    // delay to catch the new active slide
+
+    setTimeout(() => {
+      projDisplay.classList.add("play-fade-out");
+      projDisplay.innerHTML = ""; // clear container
+      if (parent == "mobile")
+        var children = Array.from(carouselMobile.children);
+      else var children = Array.from(projSideBar.children);
+
+      let active = children.filter((element) =>
+        element.classList.contains(
+          parent == "mobile" ? "active" : "active-project"
+        )
+      )[0].innerText;
+      active = projects.filter((project) => project.name == active);
+      let src = active[0].source;
+
+      buildIframe(projDisplay, src);
+
+      // fade in effect
+      projDisplay.style.animation = "fade-in 0.8s linear forwards";
+      projDisplay.style.webkitAnimation = "fade-in 0.8s linear forwards";
+
+      // Displaying its about section
+      aboutProj.innerHTML = active[0].about;
+    }, 800);
+  }
+};
